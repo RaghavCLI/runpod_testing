34f6e91699220ad4a70ad8cfe121badb45cafd62
viper-raghav <upadhyayraghav17@gmail.com>
2026-01-21 19:11:17 +0530

    remove express_server from repo

--- express_server/src/config/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const config = {
-  port: process.env.PORT || 3000,
-  ocrServiceUrl: process.env.OCR_SERVICE_URL || 'http://localhost:5000'
-};
-
-module.exports = config;
